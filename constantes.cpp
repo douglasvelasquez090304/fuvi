72dbc844bbc6cef1391925e5d682efe21b497119
Douglas Velasquez <douglasvelasquez090304@gmail.com>
2022-10-04 10:16:12 -0600

    Se ingreso la modificacion del archivo

---
+++ constantes.cpp
@@ -0,0 +1,19 @@
+/*
+Nombre del archivo: constante.cpp
+Autor: Douglas Vleasquez
+Lugar ITV
+Instrucciones: uso de las constante 
+*/
+# include <iostream>
+using namespace std;
+//variable globales
+const double pi = 3.141592
+
+int main(){
+	double radio, area;
+	 
+	 area= radio * radio;
+	 count <<"Ingrese el radio del circuito: "
+	 cin >> radio;
+	 cout <<"Ingrese area de un circuito con radio " >> radio <<"es" <<area;
+}
---
+++ pitagora.cpp
@@ -0,0 +1,21 @@
+/*
+Nombre del archivo: constante.cpp
+Autor: Douglas Vleasquez
+Lugar ITV
+Instrucciones: uso de las constante 
+*/
+# include <iostream>
+
+using namespace std;
+
+int main (){
+	double cateto1, cateto2, hipotenusa;
+	cout <<"Ingrese la hipotenusa 1:";
+	cin >> cateto1;
+	cout << "Ingrese la hipotenusa 2:";
+	cin >> cateto2;
+	hipotenusa = sqrt(pow(cateto1,2) + pow(cateto2, 2))
+	cout << "Cateto 1 = " <<cateto1 << nedl;
+		cout << "Cateto 2 = " <<cateto2 << nedl;
+		cout << "Hipotenusa = " << hipotenusa << endl;
+}
